e145920d486f6d733cb08ad65ac2c8c7463ad356
raphael.amorminio <raphael.amorminio@gmail.com>
2025-10-02 08:45:22 -0300

    first commit

---
+++ src/modules/home/home.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
